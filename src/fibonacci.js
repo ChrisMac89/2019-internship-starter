30de29499c7912a05be6628887fe065ddc5bf0a9
ChrisMac89 <chris_mac89@hotmail.co.uk>
2019-04-07 21:49:29 +0100

    Application complete

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,7 +1,29 @@
 function getSequence(n) {
-  // TODO: Implement your code to replace the line below
-  return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55].slice(0, n);
+   
+//initialises the array
+ var fib = [];
+ 
+ // variables that will be used for the fibonacci sequence
+ var a = 1, b = 0, temp;
+ 
+ //O(n) time complexity. Goes through the array N amount of times.
+ for (var i = 0; i < n; i++) 
+   {
+   //use a temporary variable to store the previous value
+    temp = a;
+
+    //set the variable 'a' to a new value
+    a = a + b;
+
+    //make b the total value in the sequence
+    b = temp;
+
+    //send the value to an array
+    fib[i] = b;
+  }
+  return fib;
 }
+
 
 module.exports = {
   getSequence
--- src/tableFormatter.js
+++ src/tableFormatter.js
@@ -1,5 +1,30 @@
 function format(table) {
+
+  //Gets the length of the table -1. 
+  var tableLength = table[table.length-1][table.length-1]; 
+
   const tableText = table.map(function(row) {
+
+    //loop through the row array
+    for (var i = 0; i < row.length; i++) {
+      
+      //translates the row array into a string
+      var store = row[i].toString();
+
+      //Padding variable for adding extra spaces if needed. 
+      var paddingForNumbers = tableLength.toString().length;
+    
+      //add padding spaces to string value
+      while(store.length < paddingForNumbers)
+        {
+          //using padStart to pad the numbers
+          store = store.padStart(store.length+1, " ");
+        }
+
+        //storing in the array
+        row[i] = store;
+    }
+
     return row.join(" ");
   });
 
